9401522222fb766cd9261a2bc947ff7febf50da5
Duy Bùi <duybui@Duys-Mac.local>
2017-07-04 23:22:20 +0700

    First commit

--- FetchImage/ViewController.swift
+++ FetchImage/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var txtLink: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func imgFetch(_ sender: Any) {
+        let imageUrl: URL = URL(string: txtLink.text!)!
+        (URLSession(configuration: URLSessionConfiguration.default)).dataTask(with: imageUrl, completionHandler: {(imageData, response, error) in
+            if let data = imageData {
+                DispatchQueue.main.async {
+                    self.imageView.image = UIImage(data: data)
+                }
+            }
+            
+        }).resume()
+    }
 
 }
 
